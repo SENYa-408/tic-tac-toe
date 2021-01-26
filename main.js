ff13ca5dca770ed96c8eea160ba58cf5d0de03bd
Senya <senya.s408@gmail.com>
2021-01-26 21:10:24 +0200

    animation added

--- main.js
+++ main.js
@@ -17,41 +17,62 @@ const reset_btn = document.querySelector(".reset");
 
 let curCombination = [[], []];
 let typeX = false; // if x => true; if o => false
+let Clicked = [];
 
 const reset = () => {
   cells.forEach((el) => {
     el.classList.remove("x");
     el.classList.remove("o");
+    el.classList.remove("win");
+    el.classList.remove("draw");
+    Clicked = [];
     curCombination = [[], []];
   });
 };
 
 const checkWinner = () => {
-  winCombinations.forEach((winCombination) => {
+  winCombinations.forEach((winCombination, i) => {
+    let winner;
     if (winCombination.every((r) => curCombination[0].indexOf(r) >= 0)) {
+      winner = 0;
       console.log("X WINNER");
     } else if (winCombination.every((r) => curCombination[1].indexOf(r) >= 0)) {
+      winner = 1;
       console.log("O WINNER");
+    }
+
+    if (winner !== undefined) {
+      cells.forEach((cell, cellIndex) => {
+        curCombination[winner].find((el, i) => {
+          if (el === cellIndex) {
+            console.log(el, cells[cellIndex - 1]);
+            cells[cellIndex - 1].classList.add("win");
+            setTimeout(reset, 3000);
+          }
+        });
+      });
     }
   });
 };
 
 cells.forEach((el, i) => {
-  el.addEventListener("click", () => {
-    let type = typeX ? "x" : "o";
+  if (!Clicked.includes(i)) {
+    el.addEventListener("click", () => {
+      Clicked.push(i);
+      let type = typeX ? "x" : "o";
 
-    if (typeX) {
-      curCombination[0].push(i + 1);
-    } else {
-      curCombination[1].push(i + 1);
-    }
+      if (typeX) {
+        curCombination[0].push(i + 1);
+      } else {
+        curCombination[1].push(i + 1);
+      }
 
-    console.log(curCombination);
-    el.classList.add(type);
-    checkWinner();
+      el.classList.add(type);
+      checkWinner();
 
-    typeX = typeX ? false : true;
-  });
+      typeX = typeX ? false : true;
+    });
+  }
 });
 
 reset_btn.addEventListener("click", reset);
